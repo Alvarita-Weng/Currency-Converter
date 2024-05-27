7f8921f8eb86bc53ec5457337e387fc63a06e684
翁欣惠 <s1101288@gm.pu.edu.tw>
2024-05-27 22:24:32 +0800

    update USD in Form1 Desinger.cs

--- CurrencyConverter/Form1.Designer.cs
+++ CurrencyConverter/Form1.Designer.cs
@@ -44,20 +44,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(51, 60);
+            this.label1.Location = new System.Drawing.Point(57, 72);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(178, 33);
+            this.label1.Size = new System.Drawing.Size(210, 40);
             this.label1.TabIndex = 0;
             this.label1.Text = "Enter money";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(2079, 563);
+            this.label2.Location = new System.Drawing.Point(2339, 676);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 15);
+            this.label2.Size = new System.Drawing.Size(103, 18);
             this.label2.TabIndex = 1;
             this.label2.Text = "Convert From";
             // 
@@ -65,10 +65,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(37, 134);
+            this.label3.Location = new System.Drawing.Point(42, 161);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(192, 33);
+            this.label3.Size = new System.Drawing.Size(226, 40);
             this.label3.TabIndex = 2;
             this.label3.Text = "Convert From";
             // 
@@ -76,10 +76,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(55, 200);
+            this.label4.Location = new System.Drawing.Point(62, 240);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(154, 33);
+            this.label4.Size = new System.Drawing.Size(180, 40);
             this.label4.TabIndex = 3;
             this.label4.Text = "Convert To";
             // 
@@ -87,19 +87,19 @@
             // 
             this.aDisplay.AutoSize = true;
             this.aDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.aDisplay.Location = new System.Drawing.Point(16, 351);
+            this.aDisplay.Location = new System.Drawing.Point(18, 421);
             this.aDisplay.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.aDisplay.Name = "aDisplay";
-            this.aDisplay.Size = new System.Drawing.Size(229, 29);
+            this.aDisplay.Size = new System.Drawing.Size(272, 32);
             this.aDisplay.TabIndex = 4;
             this.aDisplay.Text = "Converted Amount : ";
             // 
             // aAmount
             // 
-            this.aAmount.Location = new System.Drawing.Point(261, 68);
-            this.aAmount.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.aAmount.Location = new System.Drawing.Point(294, 82);
+            this.aAmount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.aAmount.Name = "aAmount";
-            this.aAmount.Size = new System.Drawing.Size(240, 25);
+            this.aAmount.Size = new System.Drawing.Size(270, 29);
             this.aAmount.TabIndex = 5;
             // 
             // aFromCombo1
@@ -108,11 +108,12 @@
             this.aFromCombo1.Items.AddRange(new object[] {
             "Zlote",
             "Qar",
-            "Euro"});
-            this.aFromCombo1.Location = new System.Drawing.Point(261, 134);
-            this.aFromCombo1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            "Euro",
+            "USD"});
+            this.aFromCombo1.Location = new System.Drawing.Point(294, 161);
+            this.aFromCombo1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.aFromCombo1.Name = "aFromCombo1";
-            this.aFromCombo1.Size = new System.Drawing.Size(240, 23);
+            this.aFromCombo1.Size = new System.Drawing.Size(270, 26);
             this.aFromCombo1.TabIndex = 6;
             this.aFromCombo1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -123,20 +124,20 @@
             "Zlote",
             "Qar",
             "Euro"});
-            this.aToCombo2.Location = new System.Drawing.Point(261, 205);
-            this.aToCombo2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.aToCombo2.Location = new System.Drawing.Point(294, 246);
+            this.aToCombo2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.aToCombo2.Name = "aToCombo2";
-            this.aToCombo2.Size = new System.Drawing.Size(240, 23);
+            this.aToCombo2.Size = new System.Drawing.Size(270, 26);
             this.aToCombo2.TabIndex = 7;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.White;
             this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(176, 268);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Location = new System.Drawing.Point(198, 322);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(260, 46);
+            this.button1.Size = new System.Drawing.Size(292, 55);
             this.button1.TabIndex = 8;
             this.button1.Text = "Convert";
             this.button1.UseVisualStyleBackColor = false;
@@ -144,11 +145,11 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.Coral;
-            this.ClientSize = new System.Drawing.Size(611, 513);
+            this.ClientSize = new System.Drawing.Size(687, 616);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.aToCombo2);
             this.Controls.Add(this.aFromCombo1);
@@ -159,7 +160,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
